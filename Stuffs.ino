017006d7c15ce4b6a9893f4f32a2bce532e229ca
Jason <jbot801@gmail.com>
2017-03-25 13:11:25 -0400

    Tic Tac Toe Game Thing

---
+++ Stuffs.ino
@@ -0,0 +1,18 @@
+3 5 6 9 10 11
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+#define EMPTY 0;
+int board[] = {{EMPTY,EMTPY,0}
+               {0,0,0}
+               {0,0,0}}
+
+
+void loop() {
+
+}
